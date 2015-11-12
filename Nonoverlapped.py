5c907faf0e328ae9b5069d1ecae171cd519ce954
richardwxn <richardwxn@gmail.com>
2015-11-11 21:10:25 -0600

    edit

--- Nonoverlapped.py
+++ Nonoverlapped.py
@@ -1,7 +1,7 @@
 import numpy
 from sklearn.metrics import confusion_matrix
-
-def load_data():
+import time
+def load_data(xsize,ysize):
     train_labels = []
     with open('/Users/newuser/Downloads/digitdata/traininglabels', 'rb') as f:
         for i, line in enumerate(f):
@@ -16,11 +16,16 @@ def load_data():
                     train_x[i * 28 + j] = 1
 
     train_x = train_x.reshape((train_labels.shape[0], 28 * 28))
-    new_train_x = numpy.zeros((train_labels.shape[0], 14 * 14))
+    new_train_x = numpy.zeros((train_labels.shape[0], (28/xsize) * (28/ysize)))
     cnt = 0
     for i in xrange(0, 28 * 28, 2):
         if (i+1) % 28 != 0 and i / 28 % 2 == 0:
-            new_train_x[:, cnt] = train_x[:, i] + train_x[:, i+1]*2 + train_x[:, i+28]*4 + train_x[:, i+29]*8
+            num=0
+            for row in xrange(xsize):
+                for col in xrange(ysize):
+                    new_train_x[:, cnt] += pow(2,num)*train_x[:, i+28*row+col]
+                    num+=1
+            # new_train_x[:, cnt] = train_x[:, i] + train_x[:, i+1]*2 + train_x[:, i+28]*4 + train_x[:, i+29]*8
             cnt += 1
 
 
@@ -40,11 +45,16 @@ def load_data():
                     test_x[i * 28 + j] = 1
 
     test_x = test_x.reshape((test_labels.shape[0], 28 * 28))
-    new_test_x = numpy.zeros((test_labels.shape[0], 14 * 14))
+    new_test_x = numpy.zeros((test_labels.shape[0], (28/xsize) * (28/ysize)))
     cnt = 0
     for i in xrange(0, 28 * 28, 2):
         if (i+1) % 28 != 0 and i / 28 % 2 == 0:
-            new_test_x[:, cnt] = test_x[:, i] + test_x[:, i+1]*2 + test_x[:, i+28]*4 + test_x[:, i+29]*8
+            num=0
+            for row in xrange(xsize):
+                for col in xrange(ysize):
+                    new_test_x[:, cnt] += pow(2,num)*test_x[:, i+28*row+col]
+                    num+=1
+            # new_test_x[:, cnt] = test_x[:, i] + test_x[:, i+1]*2 + test_x[:, i+28]*4 + test_x[:, i+29]*8
             cnt += 1
 
     test_x = numpy.array(new_test_x, dtype=int)
@@ -53,13 +63,16 @@ def load_data():
     return train_x, train_labels, test_x, test_labels
 
 class BayesClassifier(object):
-    def __init__(self):
+    def __init__(self, xsize, ysize):
         self.bayesmatrix = None
+        self.valuerange=pow(2,xsize*ysize)
+        self.xsize=xsize
+        self.ysize=ysize
 
     def fit(self, X, y):
-        bayesmatrix = numpy.ones((10, 16, 14 * 14), dtype=numpy.float64)
+        bayesmatrix = numpy.ones((10, self.valuerange, (28/self.xsize) * (28/self.ysize)), dtype=numpy.float64)
         for k in xrange(10):
-            for i in xrange(16):
+            for i in xrange(self.valuerange):
                 for j in xrange(X.shape[1]):
                     bayesmatrix[k, i, j] = numpy.sum(X[y==k, j]==i)
         numclass = numpy.zeros(10)
@@ -72,32 +85,22 @@ class BayesClassifier(object):
     def predict(self, X):
         labels = []
         for i in xrange(X.shape[0]):
-            label = numpy.argmax(numpy.sum(numpy.log(self.bayesmatrix[:, 0, X[i, :]==0]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 1, X[i, :]==1]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 2, X[i, :]==2]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 3, X[i, :]==3]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 4, X[i, :]==4]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 5, X[i, :]==5]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 6, X[i, :]==6]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 7, X[i, :]==7]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 8, X[i, :]==8]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 9, X[i, :]==9]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 10, X[i, :]==10]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 11, X[i, :]==11]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 12, X[i, :]==12]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 13, X[i, :]==13]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 14, X[i, :]==14]), axis=1) +
-                                numpy.sum(numpy.log(self.bayesmatrix[:, 15, X[i, :]==15]), axis=1))
+            hehe=reduce(lambda x, y: x+y, [numpy.sum(numpy.log(self.bayesmatrix[:, k, X[i, :]==k]), axis=1) for k in xrange(self.valuerange)])
+            label=numpy.argmax(hehe)
             labels.append(label)
         return numpy.array(labels)
 
 
 if "__main__" == __name__:
-    X, y, test_x, test_y = load_data()
-    clf = BayesClassifier()
-    clf.fit(X, y)
-    pr = clf.predict(test_x)
-    print "Confusion Matrix"
-    print confusion_matrix(test_y, pr)
-    print "Accuracy"
-    print numpy.sum(pr == test_y) / float(test_y.shape[0])
+    size=numpy.array([2,2,2,4]).reshape((2,2))
+    for combination in size:
+        start_time=time.time()
+        X, y, test_x, test_y = load_data(combination[0], combination[1])
+        clf = BayesClassifier(combination[0],combination[1])
+        clf.fit(X, y)
+        pr = clf.predict(test_x)
+        print(time.time()-start_time)
+        print "Confusion Matrix"
+        print confusion_matrix(test_y, pr)
+        print "Accuracy"
+        print numpy.sum(pr == test_y) / float(test_y.shape[0])
--- overlapped.py
+++ overlapped.py
@@ -4,7 +4,8 @@ import time
 import numpy
 from sklearn.metrics import confusion_matrix
 
-def load_data(xsize,ysize):
+
+def load_data(xsize, ysize):
     train_labels = []
     with open('/Users/newuser/Downloads/digitdata/traininglabels', 'rb') as f:
         for i, line in enumerate(f):
@@ -19,16 +20,17 @@ def load_data(xsize,ysize):
                     train_x[i * 28 + j] = 1
 
     train_x = train_x.reshape((train_labels.shape[0], 28 * 28))
-    new_train_x = numpy.zeros((train_labels.shape[0], (28-xsize+1) * (28-ysize+1)))
-    for i in xrange((28-xsize+1) * (28-ysize+1)):
+    new_train_x = numpy.zeros(
+        (train_labels.shape[0], (28 - xsize + 1) * (28 - ysize + 1)))
+    for i in xrange((28 - xsize + 1) * (28 - ysize + 1)):
         if i % 28 != 0:
-            cnt=0
+            cnt = 0
             for row in xrange(xsize):
                 for col in xrange(ysize):
-                    new_train_x[:, i] += pow(2,cnt)*train_x[:, i+28*row+col]
-                    cnt+=1
+                    new_train_x[
+                        :, i] += pow(2, cnt) * train_x[:, i + 28 * row + col]
+                    cnt += 1
             # new_train_x[:, i] = train_x[:, i] + train_x[:, i+1]*2 + train_x[:, i+28]*4 + train_x[:, i+29]*8+train_x[:,i+2]*16+train_x[:,i+3]*32+train_x[:, i+30]*64 + train_x[:, i+31]*128
-
 
     train_x = numpy.array(new_train_x, dtype=int)
 
@@ -46,38 +48,42 @@ def load_data(xsize,ysize):
                     test_x[i * 28 + j] = 1
 
     test_x = test_x.reshape((test_labels.shape[0], 28 * 28))
-    new_test_x = numpy.zeros((test_labels.shape[0], (28-xsize+1) * (28-ysize+1)))
-    for i in xrange((28-xsize+1) * (28-ysize+1)):
+    new_test_x = numpy.zeros(
+        (test_labels.shape[0], (28 - xsize + 1) * (28 - ysize + 1)))
+    for i in xrange((28 - xsize + 1) * (28 - ysize + 1)):
         if i % 28 != 0:
-            cnt=0
+            cnt = 0
             for row in xrange(xsize):
                 for col in xrange(ysize):
-                    new_test_x[:, i] += pow(2,cnt)*test_x[:, i+28*row+col]
-                    cnt+=1
+                    new_test_x[
+                        :, i] += pow(2, cnt) * test_x[:, i + 28 * row + col]
+                    cnt += 1
                     # test_x[:, i] + test_x[:, i+1]*2 + test_x[:, i+28]*4 + test_x[:, i+29]*8 + test_x[:,i+2]*16+test_x[:,i+3]*32+test_x[:, i+30]*64 + test_x[:, i+31]*128
 
     test_x = numpy.array(new_test_x, dtype=int)
 
-
     return train_x, train_labels, test_x, test_labels
 
+
 class BayesClassifier(object):
-    def __init__(self,xsize,ysize):
+
+    def __init__(self, xsize, ysize):
         self.bayesmatrix = None
-        self.valuerange=pow(2,xsize*ysize)
-        self.xsize=xsize
-        self.ysize=ysize
+        self.valuerange = pow(2, xsize * ysize)
+        self.xsize = xsize
+        self.ysize = ysize
 
     def fit(self, X, y):
         print(self.valuerange)
-        bayesmatrix = numpy.ones((10, self.valuerange, (28-self.xsize+1) * (28-self.ysize+1)), dtype=numpy.float64)
+        bayesmatrix = numpy.ones(
+            (10, self.valuerange, (28 - self.xsize + 1) * (28 - self.ysize + 1)), dtype=numpy.float64)
         for k in xrange(10):
             for i in xrange(self.valuerange):
                 for j in xrange(X.shape[1]):
-                    bayesmatrix[k, i, j] = numpy.sum(X[y==k, j]==i)
+                    bayesmatrix[k, i, j] = numpy.sum(X[y == k, j] == i)
         numclass = numpy.zeros(10)
         for i in xrange(10):
-            numclass[i] = numpy.sum(y==i) + 1
+            numclass[i] = numpy.sum(y == i) + 1
         bayesmatrix += 1.
         bayesmatrix /= numclass[:, numpy.newaxis, numpy.newaxis]
         self.bayesmatrix = bayesmatrix
@@ -85,8 +91,9 @@ class BayesClassifier(object):
     def predict(self, X):
         labels = []
         for i in xrange(X.shape[0]):
-            hehe=reduce(lambda x, y: x+y, [numpy.sum(numpy.log(self.bayesmatrix[:, k, X[i, :]==k]), axis=1) for k in xrange(self.valuerange)])
-            label=numpy.argmax(hehe)
+            hehe = reduce(lambda x, y: x + y, [numpy.sum(numpy.log(
+                self.bayesmatrix[:, k, X[i, :] == k]), axis=1) for k in xrange(self.valuerange)])
+            label = numpy.argmax(hehe)
             labels.append(label)
         return numpy.array(labels)
 
@@ -94,14 +101,14 @@ class BayesClassifier(object):
 if "__main__" == __name__:
 
     # size=numpy.array([2,2,2,4,4,2,4,4,2,3,3,2,3,3]).reshape((7,2))
-    size=numpy.array([2,2,2,4]).reshape((2,2))
+    size = numpy.array([2, 2, 2, 4]).reshape((2, 2))
     for combination in size:
-        start_time=time.time()
+        start_time = time.time()
         X, y, test_x, test_y = load_data(combination[0], combination[1])
-        clf = BayesClassifier(combination[0],combination[1])
+        clf = BayesClassifier(combination[0], combination[1])
         clf.fit(X, y)
         pr = clf.predict(test_x)
-        print(time.time()-start_time)
+        print(time.time() - start_time)
         print "Confusion Matrix"
         print confusion_matrix(test_y, pr)
         print "Accuracy"
